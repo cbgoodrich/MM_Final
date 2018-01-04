b71e3be7564c4b76d7f5c8ce1dc1c1e882020cc5
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2018-01-04 10:26:48 -0500

    Updated from Brython Server: 1/4/2018 10:26:47 AM

--- markovP4.py
+++ markovP4.py
@@ -1,1 +1,20 @@
+#Charlie Goodrich
+#01/04/18
+#markovP4.py - the fourth problem for project 5
 
+from random import randint
+
+forkliftList = ["working"]*27
+
+i = 0
+inShop = 0
+for forklift in forkliftList:
+    randNum = randint(0,132)
+    if forkliftList[i] == "broken":
+        var = 0
+    else:
+        if randNum <= 1:
+            forkliftList[i] = "broken"
+            inShop += 1
+    i += 1
+print(forkliftList)
